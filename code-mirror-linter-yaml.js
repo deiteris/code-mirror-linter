3270f298585475a35a07789ccb02c62509bbf518
Pawel <jarrodek@gmail.com>
2019-07-31 23:06:33 -0700

    refactor: upgrading to final WC spec and LitElement

--- code-mirror-linter-yaml.js
+++ code-mirror-linter-yaml.js
@@ -12,7 +12,7 @@ License for the specific language governing permissions and limitations under
 the License.
 */
 import './code-mirror-linter.js';
-import '../../codemirror/addon/lint/yaml-lint.js';
+import 'codemirror/addon/lint/yaml-lint.js';
 /**
 Import for code mirror yaml linter.
 */
--- code-mirror-linter.js
+++ code-mirror-linter.js
@@ -21,6 +21,5 @@ This file imports basic linter files. Use other import files for specific linter
 - code-mirror-linter/code-mirror-linter-json.js
 - code-mirror-linter/code-mirror-linter-yaml.js
 */
-import './lint-style.js';
-import '../../jsonlint/web/jsonlint.js';
-import '../../codemirror/addon/lint/lint.js';
+import 'jsonlint/web/jsonlint.js';
+import 'codemirror/addon/lint/lint.js';
